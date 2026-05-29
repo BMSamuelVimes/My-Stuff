a7bea2030dc41e28cd95e4aaf7878dd449f99dee
BMSamuelVimes <staylor42911@gmail.com>
2026-05-29 10:37:46 -0400

    JI Random Changes 1

--- JustImporting.py
+++ JustImporting.py
@@ -1,22 +1,34 @@
-import tkinter
+import tkinter as tk
+import random
+from tkinter import font
 
-from tkinter import *
+# from tkinter import *
 
-root = Tk()
+root = tk.Tk()
 root.title("Welcome to Thundahdome!")
 root.geometry('350x200')
 
-click_count = IntVar(value=0)
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
 
-lbl = Label(root, text = "HERE'S A BUTTON!!")
-lbl.grid()
+click_count = tk.IntVar(value=0)
+
+lbl = tk.Label(root, text = "HERE'S A BUTTON!!")
+lbl.grid(column=0, row=0, columnspan=2)
+
+big_font = ("Helvetica", 28, "bold")
+rand_lbl = tk.Label(root, text="", font=big_font, fg="blue")
+rand_lbl.grid(column=0, row=2, columnspan=2, pady=(0, 10))
 
 def clicked():
     click_count.set(click_count.get() + 1)
+    rand = random.randint(1, 100)
     lbl.configure(text = f"Clickity click! ({click_count.get()} clicks.)")
-    lbl.grid(column=0, row=1, columnspan=2)
+    rand_lbl.configure(text = str(rand))
 
-btn = Button(root, text = "CLICK IT!", fg = "purple", command=clicked)
-btn.grid(column=1, row=0)
+    # lbl.grid(column=0, row=1, columnspan=2)
+
+btn = tk.Button(root, text = "CLICK IT!", fg = "purple", command=clicked)
+btn.grid(column=0, row=1, columnspan=2)
 
 root.mainloop()
